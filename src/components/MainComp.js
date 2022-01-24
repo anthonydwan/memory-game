1e50f82ce0b26cfd579da25da057a5f230979f5a
anthony <anthonydwan@outlook.com>
2022-01-24 23:56:58 +1100

    still working on async code

--- src/components/MainComp.js
+++ src/components/MainComp.js
@@ -11,38 +11,87 @@ function MainComp() {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
-  const checkInPile = async (card) => {
-    if (pileList.length === 0) {
-      setScore((prevScore) => prevScore + 1);
-      setBestScore((prevBestScore) => Math.max(prevBestScore, score + 1));
-      console.log("CREATING PILE");
-      await addToPile(card);
-      const cardArr = await listPile();
-      setPileList(cardArr);
-    } else {
-      const cardArr = await listPile();
-      if (cardArr.map((element) => element.code).includes(card)) {
-        console.log("REPEAT!");
-        setScore(0);
-      } else {
-        setScore((prevScore) => prevScore + 1);
-        setBestScore((prevBestScore) => Math.max(prevBestScore, score + 1));
-        console.log("NOT REPEAT!");
-        await addToPile(card);
-        //
-      }
-    }
+  const addToPile = (card) => {
+    setPileList((prevPileList) => [...prevPileList, card]);
   };
 
-  useEffect(() => {
-    
-  })
+  const checkInPile = async (card) => {
+    if (pileList.includes(card)) {
+      console.log("REPEAT!");
+      setScore(0);
+    } else {
+      setScore((prevScore) => prevScore + 1);
+      setBestScore((prevBestScore) => Math.max(prevBestScore, score + 1));
+      console.log("NOT REPEAT!");
+      addToPile(card);
+    }
+    await Promise.all([addToPileAPI(card), returnToDeck(card)])
+      .then(() => printPiles())
+      .then(() => redraw());
+  };
 
-  // const drawCardFromPile = async () => {
-  //   const pileSize = await fetch()
-  //   const redrawFromPile = min(11, )
+  const returnToDeck = async (card) => {
+    await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckID}/return/?cards=${currCards
+        .map((c) => c.code)
+        .filter((c) => c !== card)
+        .join(",")}`
+    );
+  };
 
-  // }
+  const printPiles = async () => {
+    const print = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckID}/pile/drawn/list/`,
+      { mode: "cors" }
+    );
+    const printjson = await print.json();
+    await console.log(printjson);
+  };
+
+  const drawFromPileAPI = async (num, id = deckID, pileName = "drawn") => {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${id}/pile/${pileName}/draw/?count=1`,
+      { mode: "cors" }
+    );
+    const drawn = await response.json();
+    const drawnCards = await drawn;
+    return drawnCards;
+  };
+
+  const drawFromDeckAPI = async (num, id = deckID) => {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${id}/draw/?count=${num}`,
+      { mode: "cors" }
+    );
+    const drawn = await response.json();
+    const drawnCards = await drawn;
+    return drawnCards;
+  };
+
+  const redraw = async (id = deckID, pileName = "drawn") => {
+    console.log("redraw!");
+    // const fromPileNum = Math.min(pileList.length, 9);
+    // const newCards = await drawFromPileAPI(1);
+    const [drawnFromPile, drawnFromDeck] = await Promise.all([
+      drawFromPileAPI(1),
+      drawFromDeckAPI(9),
+    ]);
+    const newCards = [...drawnFromPile.cards, ...drawnFromDeck.cards];
+    console.log(newCards);
+    // setCurrCards(newCards);
+  };
+
+  const updatePileAPI = async (card, id = deckID, pileName = "drawn") => {
+    console.log(card);
+    await fetch(`https://deckofcardsapi.com/api/deck/${id}/shuffle/`);
+  };
+
+  const addToPileAPI = async (code, id = deckID, pileName = "drawn") => {
+    await fetch(
+      `https://deckofcardsapi.com/api/deck/${id}/pile/${pileName}/add/?cards=${code}`,
+      { mode: "cors" }
+    );
+  };
 
   const getDeckIDFromAPI = async () => {
     try {
@@ -54,6 +103,7 @@ function MainComp() {
       );
       const res = await response.json();
       const id = await res.deck_id;
+      console.log(id);
       setIsLoading(false);
       return id;
     } catch {
@@ -61,7 +111,7 @@ function MainComp() {
     }
   };
 
-  const drawCard = async (id = null, numCard = 10) => {
+  const initDraw = async (id = null, numCard = 10) => {
     try {
       const deckID = id === null ? await getDeckIDFromAPI() : await id;
       const response = await fetch(
@@ -77,33 +127,15 @@ function MainComp() {
     }
   };
 
-  const addToPile = async (code, id = deckID, pileName = "chosen") => {
-    await fetch(
-      `https://deckofcardsapi.com/api/deck/${id}/pile/${pileName}/add/?cards=${code}`,
-      { mode: "cors" }
-    );
-  };
-
-  const listPile = async (id = deckID, pileName = "chosen") => {
-    const response = await fetch(
-      `http://deckofcardsapi.com/api/deck/${id}/pile/${pileName}/list/`,
-      { mode: "cors" }
-    );
-    const currPileCheck = await response.json();
-    console.log(currPileCheck.piles.chosen.cards);
-    return currPileCheck.piles.chosen.cards;
-  };
-
   useEffect(() => {
     const deckID = getDeckIDFromAPI();
     deckID.then((str) => setDeckID(str));
-    drawCard(deckID, 10).then((obj) => setCurrCards(obj.cards));
+    initDraw(deckID, 10).then((obj) => setCurrCards(obj.cards));
   }, []);
 
-  const handleClick = async (e) => {
-    const chosen = await e.target.getAttribute("data-code");
-    console.log(`adding to pile ${chosen}`);
-    await checkInPile(chosen);
+  const handleClick = (e) => {
+    const chosen = e.target.getAttribute("data-code");
+    checkInPile(chosen);
   };
 
   return (
